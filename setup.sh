f92a541f67f01e271f47fe3d3b1495f3990d687a
Ramon Brooker <rbrooker@aeo3.ca>
2022-03-20 11:24:35 -0400

    all Ubuntu

--- setup.sh
+++ setup.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
+
 USED_BRANCH='bootstrap'
-if [[ ! -z /.bootstraped ]]; then touch /.bootstraped ; fi
-WHOAMI="$(whoami)" ;REPO_PREFIX="/etc/custom";REPO_PATH="${REPO_PREFIX}/configuration";SKEL="/etc/skel"
-apt-get update; apt-get upgrade -y; apt-get install -y net-tools bash-completion exuberant-ctags universal-ctags vim mlocate git pwgen bind9-dnsutils jq openssl curl rsync expect unzip zip wget tmux bc wireguard wireguard-tools iptables; ADMIN_GROUP="adm"
-if [[ ! -d "${REPO_PATH}" ]]; then if [[ ! -d ${REPO_PREFIX} ]]; then mkdir -p "${REPO_PREFIX}" ; fi; chgrp -R ${ADMIN_GROUP} ${REPO_PREFIX}; chmod g+rw ${REPO_PREFIX}; cd "${REPO_PREFIX}"; git clone --branch "${USED_BRANCH}" https://github.com/cognition/configuration.git; fi
-if [[ ! -d /etc/bash.bashrc.d ]]; then mkdir  /etc/bash.bashrc.d ; fi
+
+
+if  [[ -f /.bootstraped ]]; then
+    exit 0;
+else
+    touch /.bootstraped ;
+fi
+
+
+WHOAMI="$(whoami)";
+REPO_PREFIX="/etc/custom";
+REPO_PATH="${REPO_PREFIX}/configuration";
+SKEL="/etc/skel"
+apt-get update;
+apt-get upgrade -y;
+apt-get install -y net-tools bash-completion exuberant-ctags universal-ctags vim mlocate git pwgen bind9-dnsutils jq openssl curl rsync expect unzip zip wget tmux bc wireguard wireguard-tools iptables;
+ADMIN_GROUP="adm"
+
+if [[ ! -d "${REPO_PATH}" ]]; then
+    if [[ ! -d ${REPO_PREFIX} ]]; then
+        mkdir -p "${REPO_PREFIX}";
+    fi;
+    chgrp -R ${ADMIN_GROUP} ${REPO_PREFIX};
+    chmod g+rw ${REPO_PREFIX};
+    cd "${REPO_PREFIX}";
+    git clone --branch "${USED_BRANCH}" https://github.com/cognition/configuration.git;
+fi
+if [[ ! -d /etc/bash.bashrc.d ]]; then
+    mkdir  /etc/bash.bashrc.d;
+fi
 
 ### Populate General Bash Environment
 cp -f ${REPO_PATH}/files/etc-bash/* /etc/bash.bashrc.d/
